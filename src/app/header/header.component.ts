6a13c42d520883169c8e4759ff6fa00055d91112
umalat <serfid7@gmail.com>
2024-10-03 17:07:35 +0300

    basic-structure

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
     selector: 'app-header',
     standalone: true,
     templateUrl: './header.component.html',
-    styleUrl: './header.component.css'
+    styleUrls: ['./header.component.css',]
 
   })
 
