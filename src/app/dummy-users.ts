2e78abec38ec1675ce4171c1a740bb27ceb907c5
umalat <serfid7@gmail.com>
2024-10-09 12:51:15 +0300

    ch_23

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,18 @@
+export const DUMMY_USERS = [
+    {
+        id: 'u1',
+        name: 'Jasmine Washington',
+        avatar: 'user-1.jpg',
+    },
+    {
+        id: 'u2',
+        name: 'Emily Thompson',
+        avatar: 'user-2.jpg',
+    },
+    {
+        id: 'u3',
+        name: 'Marcus Johnson',
+        avatar: 'user-3.jpg',
+    },
+
+]
