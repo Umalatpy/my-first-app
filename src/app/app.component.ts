7583b388fdbf557e9e2af5b19606e17d6b4ca91c
umalat <serfid7@gmail.com>
2024-10-11 11:03:55 +0300

    bag fixing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,4 @@
-import { Component } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';
 
-import { HeaderComponent } from './header/header.component';
-<<<<<<< HEAD
-import { UserComponent } from "./user/user.component";
-=======
-import { UserComponent } from './user/user.component';
->>>>>>> 1cba9a11ea086b627ab87091ec432a2067d1c46f
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'my-first-app';
-}
+
